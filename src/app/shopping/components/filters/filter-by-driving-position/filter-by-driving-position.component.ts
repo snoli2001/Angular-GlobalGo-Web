e15ff29b2bcf5e93fe82fefd32670356951a71f0
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-06-28 17:28:38 -0500

    filter by driving position

--- src/app/shopping/components/filters/filter-by-driving-position/filter-by-driving-position.component.ts
+++ src/app/shopping/components/filters/filter-by-driving-position/filter-by-driving-position.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-
+import { AccordionModule } from 'primeng/accordion';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-filter-by-driving-position',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,AccordionModule],
   templateUrl: './filter-by-driving-position.component.html',
-  styleUrl: './filter-by-driving-position.component.css'
+  styleUrl: './filter-by-driving-position.component.css',
 })
 export class FilterByDrivingPositionComponent {
-
+  public icons: any[] = [
+    { class: 'flaticon-moto', label: 'recta' },
+    { class: 'flaticon-motocicleta', label: 'inclinada' },
+    { class: 'flaticon-moto-1', label: 'recostada' },
+  ];
 }
--- src/app/shopping/components/filters/filter-collection/filter-collection.component.ts
+++ src/app/shopping/components/filters/filter-collection/filter-collection.component.ts
@@ -5,10 +5,11 @@ import { FilterByBrandComponent } from '../filter-by-brand/filter-by-brand.compo
 import { FilterByColorsComponent } from '../filter-by-colors/filter-by-colors.component';
 import { FilterByCylindersComponent } from '../filter-by-cylinders/filter-by-cylinders.component';
 import { FilterByDisplacementComponent } from '../filter-by-displacement/filter-by-displacement.component';
+import { FilterByDrivingPositionComponent } from '../filter-by-driving-position/filter-by-driving-position.component';
 @Component({
   selector: 'app-filter-collection',
   standalone: true,
-  imports: [FilterByBrakesComponent,ClearFilterComponent,FilterByBrandComponent,FilterByColorsComponent,FilterByCylindersComponent,FilterByDisplacementComponent],
+  imports: [FilterByDrivingPositionComponent,FilterByBrakesComponent,ClearFilterComponent,FilterByBrandComponent,FilterByColorsComponent,FilterByCylindersComponent,FilterByDisplacementComponent],
   templateUrl: './filter-collection.component.html',
   styleUrl: './filter-collection.component.css'
 })
