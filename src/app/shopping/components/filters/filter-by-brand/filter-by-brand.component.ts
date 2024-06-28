cb174e6af39637be80c1476286e209df0d40fe38
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-06-28 15:04:13 -0500

    add: add filter by brand

--- src/app/shopping/components/filters/filter-by-brand/filter-by-brand.component.ts
+++ src/app/shopping/components/filters/filter-by-brand/filter-by-brand.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccordionModule } from 'primeng/accordion';
+import { DropdownModule } from 'primeng/dropdown';
 
 @Component({
   selector: 'app-filter-by-brand',
   standalone: true,
-  imports: [],
+  imports: [AccordionModule,DropdownModule,FormsModule],
   templateUrl: './filter-by-brand.component.html',
   styleUrl: './filter-by-brand.component.css'
 })
 export class FilterByBrandComponent {
-
+  public selectedBrand:String = "";
+  public brands: any[] = [
+    { name: 'Yamaha', code: 'YM' },
+    { name: 'Patagonia', code: 'PG' },
+    { name: 'Tvs', code: 'TV' },
+    { name: 'Honda', code: 'HN' },
+    { name: 'Toyota', code: 'TY' }
+  ];
 }
--- src/app/shopping/components/filters/filter-collection/filter-collection.component.ts
+++ src/app/shopping/components/filters/filter-collection/filter-collection.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FilterByBrakesComponent } from '../filter-by-brakes/filter-by-brakes.component';
 import { ClearFilterComponent } from '../clear-filter/clear-filter.component';
+import { FilterByBrandComponent } from '../filter-by-brand/filter-by-brand.component';
 @Component({
   selector: 'app-filter-collection',
   standalone: true,
-  imports: [FilterByBrakesComponent,ClearFilterComponent],
+  imports: [FilterByBrakesComponent,ClearFilterComponent,FilterByBrandComponent],
   templateUrl: './filter-collection.component.html',
   styleUrl: './filter-collection.component.css'
 })
