c43503df09542fcb5abb582dbf72624d719d6edd
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-06-28 18:32:22 -0500

    add: filter by type of motorcycle

--- src/app/shopping/components/filters/filter-by-type-motor-cycle/filter-by-type-motor-cycle.component.ts
+++ src/app/shopping/components/filters/filter-by-type-motor-cycle/filter-by-type-motor-cycle.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { AccordionModule } from 'primeng/accordion';
 @Component({
   selector: 'app-filter-by-type-motor-cycle',
   standalone: true,
-  imports: [],
+  imports: [AccordionModule, CommonModule],
   templateUrl: './filter-by-type-motor-cycle.component.html',
   styleUrl: './filter-by-type-motor-cycle.component.css'
 })
 export class FilterByTypeMotorCycleComponent {
-
+  public icons:any[]= [
+    { class: "flaticon-moto", label: "delivery" },
+    { class: "flaticon-motocicleta", label: "Motocicleta" },
+    { class: "flaticon-moto-1", label: "chopper" },
+    { class: "flaticon-motocicleta-1", label: "deportiva" },
+    { class: "flaticon-motorcycle-3", label: "scotter" },
+    { class: "flaticon-motorbike", label: "Motorbike" },
+    { class: "flaticon-motocross", label: "Motocross" },
+    { class: "flaticon-bike", label: "Bike" }
+]
 }
