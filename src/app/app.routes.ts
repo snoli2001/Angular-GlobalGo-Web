22a9b7becb0e4b852312d6e58f126637e6895c89
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-06-28 10:47:19 -0500

    dada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { NotFoundPageComponent } from './public/pages/not-found-page/not-found-page.component';
+import { MotorcycleCatalougeComponent } from './shopping/pages/motorcycle-catalouge/motorcycle-catalouge.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },  // Redirección a la página de inicio
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    {path:'catalogo-motos',component: MotorcycleCatalougeComponent},
     {path:'**', component:NotFoundPageComponent}
 ];
