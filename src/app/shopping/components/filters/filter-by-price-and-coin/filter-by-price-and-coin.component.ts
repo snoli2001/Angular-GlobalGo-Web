f3db007bfe616f4ca9b32f3a207309a680c8878e
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-07-04 16:26:10 -0500

    add: fitler by price design

--- src/app/shopping/components/filters/filter-by-price-and-coin/filter-by-price-and-coin.component.ts
+++ src/app/shopping/components/filters/filter-by-price-and-coin/filter-by-price-and-coin.component.ts
@@ -4,14 +4,16 @@ import { SliderModule } from 'primeng/slider';
 import { ClearFilterComponent } from '../clear-filter/clear-filter.component';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
+import { FloatLabelModule } from 'primeng/floatlabel';
 
 @Component({
   selector: 'app-filter-by-price-and-coin',
   standalone: true,
-  imports: [AccordionModule,SliderModule,ClearFilterComponent,InputTextModule,FormsModule],
+  imports: [FloatLabelModule,AccordionModule,SliderModule,ClearFilterComponent,InputTextModule,FormsModule],
   templateUrl: './filter-by-price-and-coin.component.html',
   styleUrl: './filter-by-price-and-coin.component.css'
 })
 export class FilterByPriceAndCoinComponent {
-  public price:number=0;
+  public rangeValues: number[] = [20, 80];
+
 }
