9ebf9d4c9d89b83ceb30756e9a2fae4cc09e143d
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-07-04 18:32:29 -0500

    add: second option for grid

--- src/app/shopping/components/motorcycles/moto-card/moto-card.component.ts
+++ src/app/shopping/components/motorcycles/moto-card/moto-card.component.ts
@@ -2,14 +2,16 @@ import { Component, Input } from '@angular/core';
 import { ImageModule } from 'primeng/image';
 import { IMotorcycle } from '../../../models/Motorcycle';
 import { TagModule } from 'primeng/tag';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-moto-card',
   standalone: true,
-  imports: [TagModule,ImageModule],
+  imports: [TagModule,ImageModule,CommonModule],
   templateUrl: './moto-card.component.html',
   styleUrl: './moto-card.component.css'
 })
 export class MotoCardComponent {
   @Input() data: IMotorcycle | undefined;
+  @Input() option: number | undefined;
 }
