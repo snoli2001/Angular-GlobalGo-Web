e9d458b021e0bd637c45fe1711d881d562b0f7d2
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-07-04 15:30:02 -0500

    add: filter by origins

--- src/app/shopping/components/filters/filter-section-group/filter-section-group.component.ts
+++ src/app/shopping/components/filters/filter-section-group/filter-section-group.component.ts
@@ -4,10 +4,12 @@ import { FilterByPriceAndCoinComponent } from '../filter-by-price-and-coin/filte
 import { FilterByBrandComponent } from '../filter-by-brand/filter-by-brand.component';
 import { FilterByRatingComponent } from '../filter-by-rating/filter-by-rating.component';
 import { FilterByManufacturesComponent } from '../filter-by-manufactures/filter-by-manufactures.component';
+import { FilterByColorsComponent } from '../filter-by-colors/filter-by-colors.component';
+import { FilterByOriginComponent } from '../filter-by-origin/filter-by-origin.component';
 @Component({
   selector: 'app-filter-section-group',
   standalone: true,
-  imports: [FilterByManufacturesComponent,FilterByRatingComponent,FilterByBrandComponent,AccordionModule, FilterByPriceAndCoinComponent],
+  imports: [FilterByOriginComponent,FilterByColorsComponent,FilterByManufacturesComponent,FilterByRatingComponent,FilterByBrandComponent,AccordionModule, FilterByPriceAndCoinComponent],
   templateUrl: './filter-section-group.component.html',
   styleUrl: './filter-section-group.component.css'
 })
