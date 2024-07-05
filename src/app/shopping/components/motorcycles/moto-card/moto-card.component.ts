a54ef2266a409425f5e8607652410354f949a2b7
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-07-05 10:07:01 -0500

    add: router link for cards

--- src/app/shopping/components/motorcycles/moto-card/moto-card.component.ts
+++ src/app/shopping/components/motorcycles/moto-card/moto-card.component.ts
@@ -3,11 +3,12 @@ import { ImageModule } from 'primeng/image';
 import { IMotorcycle } from '../../../models/Motorcycle';
 import { TagModule } from 'primeng/tag';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-moto-card',
   standalone: true,
-  imports: [TagModule,ImageModule,CommonModule],
+  imports: [TagModule,ImageModule,CommonModule,RouterLink],
   templateUrl: './moto-card.component.html',
   styleUrl: './moto-card.component.css'
 })
