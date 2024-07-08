67e7bfadaed235ea37aca2bf1245a8d3685a80e3
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-07-08 09:58:05 -0500

    add props to compoenntes

--- src/app/shopping/components/motorcycles/motorcycle-section/motorcycle-section.component.ts
+++ src/app/shopping/components/motorcycles/motorcycle-section/motorcycle-section.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { AdvertisingComponent } from '../../../../public/components/advertising/advertising.component';
 import { FilterByDisplacementAndPriceComponent } from '../../filters/filter-by-displacement-and-price/filter-by-displacement-and-price.component';
 import { ListMotosComponent } from '../list-motos/list-motos.component';
+import { IMotorcycle } from '../../../models/Motorcycle';
 @Component({
   selector: 'app-motorcycle-section',
   standalone: true,
@@ -10,5 +11,5 @@ import { ListMotosComponent } from '../list-motos/list-motos.component';
   styleUrl: './motorcycle-section.component.css'
 })
 export class MotorcycleSectionComponent {
-
+  @Input() listMotos: IMotorcycle[] | undefined;
 }
--- src/app/shopping/pages/motorcycle-catalouge/motorcycle-catalouge.component.ts
+++ src/app/shopping/pages/motorcycle-catalouge/motorcycle-catalouge.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FilterCollectionComponent } from '../../components/filters/filter-collection/filter-collection.component';
 import { MotorcycleSectionComponent } from '../../components/motorcycles/motorcycle-section/motorcycle-section.component';
+import { IMotorcycle } from '../../models/Motorcycle';
+import { MotorCycleService } from '../../services/motorcycles.service';
 @Component({
   selector: 'app-motorcycle-catalouge',
   standalone: true,
@@ -9,5 +11,24 @@ import { MotorcycleSectionComponent } from '../../components/motorcycles/motorcy
   styleUrl: './motorcycle-catalouge.component.css'
 })
 export class MotorcycleCatalougeComponent {
+  motorCycles: IMotorcycle[] = []; 
+  layout: string = 'list';
+  first = 0;
+  constructor(private service:MotorCycleService){
 
+  }
+
+  ngOnInit(): void {
+    this.getMotorCycles();
+  }
+
+
+
+  async getMotorCycles(): Promise<void> {
+    try {
+      this.motorCycles = await this.service.getMotorCycles(); 
+    } catch (error) {
+      console.error('Error fetching motorcycles:', error); 
+    }
+  }
 }
