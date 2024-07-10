8e737ad4c75e7d38e17645c0399bbeb5055906ba
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-07-10 14:37:25 -0500

    add: valor en soles

--- src/app/shopping/components/motorcycles/moto-header-detail/moto-header-detail.component.ts
+++ src/app/shopping/components/motorcycles/moto-header-detail/moto-header-detail.component.ts
@@ -1,18 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { IMotorcycle } from '../../../models/Motorcycle';
 
 @Component({
   selector: 'app-moto-header-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './moto-header-detail.component.html',
   styleUrl: './moto-header-detail.component.css'
 })
 export class MotoHeaderDetailComponent {
   @Input() motorcycle: IMotorcycle | undefined;
-  public today:Date;
+  public today: Date;
 
-  constructor(){
-    this.today = new Date;
+  constructor() {
+    this.today = new Date();
   }
 }
