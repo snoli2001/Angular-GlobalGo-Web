ca2465034b03e9e70f3a4943c92c2493bffd63e7
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-07-19 09:15:58 -0500

    add: features

--- src/app/shopping/components/motorcycles/chispa-information/chispa-information.component.ts
+++ src/app/shopping/components/motorcycles/chispa-information/chispa-information.component.ts
@@ -1,13 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { IMotorcycle } from '../../../models/Motorcycle';
+import { DialogModule } from 'primeng/dialog';
 
 @Component({
   selector: 'app-chispa-information',
   standalone: true,
-  imports: [],
+  imports: [DialogModule],
   templateUrl: './chispa-information.component.html',
   styleUrl: './chispa-information.component.css'
 })
 export class ChispaInformationComponent {
   @Input() motorcycle:IMotorcycle | undefined;
+  visible: boolean = false;
+
+  dialogHandle(){
+    this.visible = !this.visible;
+  }
 }
