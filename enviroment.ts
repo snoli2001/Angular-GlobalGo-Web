cfc41c8e000d620d41d9d03ce7f2c76f52723fdb
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-07-31 10:58:16 -0500

    add: url api added

--- enviroment.ts
+++ enviroment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7092/api'
+    // apiUrl: 'https://localhost:7092/api'
+    apiUrl: 'http://172.24.134.11:9852/api'
   };
   
