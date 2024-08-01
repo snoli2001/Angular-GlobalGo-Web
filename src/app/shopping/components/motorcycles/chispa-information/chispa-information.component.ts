33b6ad11d59becdb002da0637530e8b323e81dd6
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-08-01 12:48:34 -0500

    adadslkand

--- src/app/shopping/components/motorcycles/chispa-information/chispa-information.component.ts
+++ src/app/shopping/components/motorcycles/chispa-information/chispa-information.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { IMotorcycle } from '../../../models/Motorcycle';
 import { DialogModule } from 'primeng/dialog';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-chispa-information',
@@ -12,6 +13,14 @@ import { DialogModule } from 'primeng/dialog';
 export class ChispaInformationComponent {
   @Input() motorcycle:IMotorcycle | undefined;
   visible: boolean = false;
+  public motoId:number;
+  public financingUrl: string;
+
+  constructor(private route: ActivatedRoute){
+    const id = this.route.snapshot.paramMap.get('id');
+    this.motoId = id ? parseInt(id, 10) : 0;
+    this.financingUrl = `https://globalgo-login.sis360.com.pe/solicitar-financiamiento?Id=${this.motoId}`;
+  }
 
   dialogHandle(){
     this.visible = !this.visible;
