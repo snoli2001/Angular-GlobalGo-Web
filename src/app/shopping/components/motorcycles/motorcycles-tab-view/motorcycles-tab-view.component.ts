968ed1a62f58d8f34f878ed7aae77fdf4687aff2
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-08-08 10:25:40 -0500

    add: velocidad validacion

--- src/app/shopping/components/motorcycles/motorcycles-tab-view/motorcycles-tab-view.component.ts
+++ src/app/shopping/components/motorcycles/motorcycles-tab-view/motorcycles-tab-view.component.ts
@@ -2,16 +2,20 @@ import { Component, Input } from '@angular/core';
 import { IMotorcycle } from '../../../models/Motorcycle';
 import { TabViewModule } from 'primeng/tabview';
 import { AccordionModule } from 'primeng/accordion';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-motorcycles-tab-view',
   standalone: true,
-  imports: [TabViewModule,AccordionModule],
+  imports: [TabViewModule,AccordionModule,CommonModule],
   templateUrl: './motorcycles-tab-view.component.html',
   styleUrl: './motorcycles-tab-view.component.css'
 })
 export class MotorcyclesTabViewComponent {
   @Input() motorcycle: IMotorcycle | undefined;
 
+  probar(){
+    console.log(this.motorcycle);
+  }
 }
