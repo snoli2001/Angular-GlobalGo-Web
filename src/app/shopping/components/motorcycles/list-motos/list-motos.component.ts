7dcb64c89426c4f4ebe3ef6bcadd630dc80135f4
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-08-12 16:25:20 -0500

    fix: dataview of motorcycles

--- src/app/shopping/components/motorcycles/list-motos/list-motos.component.ts
+++ src/app/shopping/components/motorcycles/list-motos/list-motos.component.ts
@@ -4,19 +4,25 @@ import { IMotorcycle } from '../../../models/Motorcycle';
 import { CommonModule } from '@angular/common';
 import { ScrollPanelModule } from 'primeng/scrollpanel';
 import { DataViewModule } from 'primeng/dataview';
-
+import { PaginatorModule } from 'primeng/paginator';
 @Component({
   selector: 'app-list-motos',
   standalone: true,
-  imports: [CommonModule,MotoCardComponent, MotoCardComponent,ScrollPanelModule,DataViewModule],
+  imports: [CommonModule,MotoCardComponent, MotoCardComponent,ScrollPanelModule,DataViewModule,PaginatorModule],
   templateUrl: './list-motos.component.html',
   styleUrl: './list-motos.component.css'
 })
 export class ListMotosComponent {
   @Input() listMotorcycles: IMotorcycle[];
-
+  public option:number;
   constructor(){
+    this.option = 1;
     this.listMotorcycles = [];
   }
-
+  handleOption1(){
+    this.option = 1;
+  }
+  handleOption2(){
+    this.option = 2;
+  }
 }
