68b379daff82b5a76867539d524e334b0fd85ba9
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-08-14 18:49:53 -0500

    fix: videplayer css

--- src/app/shopping/components/motorcycles/motorcycle-video/motorcycle-video.component.ts
+++ src/app/shopping/components/motorcycles/motorcycle-video/motorcycle-video.component.ts
@@ -23,25 +23,17 @@ export class MotorcycleVideoComponent {
 
   handlevideo() {
     if (this.showVideo) {
-      this.videoClass = 'elevate-video';
-      this.sectionClass = 'hidden';
-      setTimeout(() => {
-        this.showVideo = false;
-        this.videoClass = '';
-      }, 1000);
+      this.showVideo = false;
+      this.videoClass = 'descend-video';
     } else {
       this.showVideo = true;
       this.videoClass = 'elevate-video';
-      this.sectionClass = 'block';
-      setTimeout(() => this.initializePlayer(), 100); 
     }
   }
+  
 
   ngAfterViewInit() {
-    if (this.showVideo) {
-      window.alert(this.showVideo);
-      this.initializePlayer();
-    }
+    this.initializePlayer();
   }
 
   initializePlayer() {
