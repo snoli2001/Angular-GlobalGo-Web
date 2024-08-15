35a69a51636db96204fefe70582f7f7fbebcf290
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-08-15 10:53:37 -0500

    fix: error

--- src/app/shopping/pages/motorcycle-detail/motorcycle-detail.component.ts
+++ src/app/shopping/pages/motorcycle-detail/motorcycle-detail.component.ts
@@ -5,7 +5,7 @@ import { CarrouselForMotorcyclesComponent } from '../../components/motorcycles/c
 import { IMotorcycle } from '../../models/Motorcycle';
 import { MotoStateService } from '../../states/moto.state.service';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { DealersStoresMapComponent } from '../../components/motorcycles/dealers-stores-map/dealers-stores-map.component';
 import { MotorcycleVideoComponent } from '../../components/motorcycles/motorcycle-video/motorcycle-video.component';
 
@@ -18,42 +18,21 @@ import { MotorcycleVideoComponent } from '../../components/motorcycles/motorcycl
 })
 export class MotorcycleDetailComponent {
   motorCycle: IMotorcycle | null = null;
-  private timeoutId: any;
+  constructor(private serviceState: MotoStateService, private route: ActivatedRoute){}
 
-  constructor(
-    private serviceState: MotoStateService,
-    private route: ActivatedRoute,
-    private router: Router
-  ) {}
   ngOnInit(): void {
-    this.route.params.subscribe((params) => {
-      const motoId = +params['id'];
+    this.route.params.subscribe(params => {
+      const motoId = +params['id']; 
       this.serviceState.getMotorcycleByID(motoId);
     });
 
     this.serviceState.moto$.subscribe({
       next: (moto) => {
         this.motorCycle = moto;
-        if (this.motorCycle) {
-          clearTimeout(this.timeoutId);
-        }
       },
       error: (error) => {
         console.log('Error fetching this motorcycle', error);
-      },
-    });
-    this.timeoutId = setTimeout(() => {
-      if (!this.motorCycle) {
-        this.router.navigate(['/not-found']);
       }
-    }, 5000);
+    });
   }
-
-  ngOnDestroy() {
-    // Limpiamos el temporizador si el componente se destruye
-    if (this.timeoutId) {
-      clearTimeout(this.timeoutId);
-    }
-  }
-
 }
