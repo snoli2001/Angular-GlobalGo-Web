785869dde6d3aab51fd0b11c980247c231923c33
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-09-04 11:15:37 -0500

    add: new api link for dev tunnels

--- enviroment.ts
+++ enviroment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7092/api'
+    // apiUrl: 'https://localhost:7092/api',
+    apiUrl: 'https://wc70k9xw-7092.brs.devtunnels.ms/api',
     // apiUrl: 'https://api-catalogo.sis360.com.pe/api'
   };
   
