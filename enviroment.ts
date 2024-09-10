56988ac779f60246353b70b7a0c35bd0580989ff
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-09-10 10:32:41 -0500

    add:

--- enviroment.ts
+++ enviroment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     // apiUrl: 'https://localhost:7092/api',
-    apiUrl: 'https://jf0r7phr-7092.brs.devtunnels.ms/api',
+    apiUrl: 'https://n501h324-7092.brs.devtunnels.ms/api',
     // apiUrl: 'https://api-catalogo.sis360.com.pe/api'
   };
   
